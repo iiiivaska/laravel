d43cb676e7fc351d17f579d8bd46aae38babd8b3
iiiivaska <iiiivaska@yandex.ru>
2022-05-17 14:53:05 +0300

    Сделан вывод сообщений на странице Сообщения

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 
-@section('title-block')Главная страница@endsection
+@section('title-block')Все сообщения@endsection
 
 @section('content')
     <h1>Главная страница</h1>
-    <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium alias atque beatae eligendi hic ipsa ipsum itaque nam non provident, saepe sed soluta voluptate! Esse ex inventore ipsa odit quos?</span><span>Delectus dicta, sequi. Aliquid consectetur cupiditate, delectus deleniti deserunt distinctio doloribus earum eius esse et fuga illo magnam maiores molestiae nam nesciunt nobis obcaecati quos rem sequi soluta temporibus voluptas!</span><span>Accusamus alias at consequuntur dolorum, eius enim eos error facilis harum illum inventore itaque labore magni modi nemo neque nostrum quasi quia quos reprehenderit saepe sed tempore temporibus tenetur voluptate?</span><span>Accusantium amet atque blanditiis dicta esse eveniet facilis illo inventore ipsum iure iusto labore libero magnam maiores omnis optio, quasi quibusdam quisquam recusandae rem saepe sed sunt velit veniam voluptatum.</span><span>Alias architecto asperiores assumenda corporis cum, dolore dolorem dolorum ducimus eligendi eveniet ipsa ipsam laboriosam magnam natus neque odit officiis placeat porro quaerat quas quos repudiandae sapiente tempore tenetur ullam.</span><span>Ab assumenda aut deserunt dolores earum eum ex facilis hic iure iusto labore minus modi nisi nostrum officia omnis quas quibusdam, quisquam recusandae reiciendis sunt suscipit tempora velit vero voluptatibus?</span><span>Aliquid iusto, sunt. Ad delectus distinctio impedit laborum maiores optio, quia quisquam sit voluptatum. A amet culpa cumque eaque earum eos illum, molestiae neque nostrum porro similique sint veniam voluptatibus!</span><span>Adipisci, blanditiis consequatur deleniti ducimus eius fugit laudantium natus repudiandae! Aliquam asperiores corporis cum cupiditate eveniet facilis id natus, nobis quo ratione repellendus sequi temporibus tenetur ullam voluptas voluptatibus voluptatum?</span><span>Cumque eos eum hic veritatis. Amet animi aspernatur at culpa debitis dolor ea facere facilis fugit illo impedit incidunt iure molestiae natus, nisi officiis quos recusandae rem. Omnis, pariatur, similique.</span><span>Aperiam beatae doloremque eaque inventore quas? Accusamus aliquam asperiores dolores ea enim hic nam quos saepe sapiente sed. Accusantium dolorem ducimus error, facilis hic magni numquam rerum. Blanditiis, dolorum ipsa!</span></p>
+    @foreach($data as $element)
+        <div class="alert alert-info">
+            <h3>{{ $element->subject }}</h3>
+            <p>{{ $element->email }}</p>
+            <p>{{ $element->created_at }}</p>
+        </div>
+    @endforeach
 @endsection
 
-@section('aside')
-    @parent
-    <p>Дополнительный текст</p>
-@endsection
